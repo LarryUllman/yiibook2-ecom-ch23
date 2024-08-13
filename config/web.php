e31e8a2f3692d02588588b038bc47f6ca9c01f4e
Larry Ullman <larry@larryullman.com>
2024-08-13 11:29:54 -0400

    initial edits

--- config/web.php
+++ config/web.php
@@ -4,7 +4,7 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic',
+    'id' => 'Buy My Books!',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'rfiAEFhl9P',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -42,14 +42,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
